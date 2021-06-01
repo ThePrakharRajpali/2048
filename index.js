a11397debf704bc240b2900b3f48ac32c2086db5
Prakhar <rajpaliprakhar23@gmail.com>
2021-06-01 18:43:03 +0530

    completed goright and goup functions

--- index.js
+++ index.js
@@ -60,29 +60,77 @@ class Board {
     goRight() {
         for(var j=3; j>-1;j--){
             for(var i=0; i<4; i++){
-                while(j !== 3 && (this.grid[i][j+1].tileValue() == 0 || this.grid[i][j+1].tileValue() == this.grid[i][j].tileValue())){
-                    if(this.grid[i][j+1].tileValue() == this.grid[i][j].tileValue()){
-                        var nextVal = this.grid[i][j+1].tileValue();
+                if(this.grid[i][j].tileValue() !== 0){
+                    var k = j;
+                    while(k !== 3 && (this.grid[i][k+1].tileValue() == 0 || this.grid[i][k+1].tileValue() == this.grid[i][k].tileValue())){
+                        if(this.grid[i][k+1].tileValue() == this.grid[i][j].tileValue()){
+                            var nextVal = this.grid[i][k+1].tileValue();
 
-                        this.grid[i][j+1].tileValue(nextVal * 2);
-                        this.grid[i][j+1].tileValue(0);
-                    } else if(this.grid[i][j+1].tileValue() == 0) {
-                        var currVal = this.grid[i][j].tileValue();
+                            this.grid[i][k+1].tileValue(nextVal * 2);
+                            this.grid[i][k+1].tileValue(0);
+                            k++;
+                        } else if(this.grid[i][k+1].tileValue() == 0) {
+                            var currVal = this.grid[i][k].tileValue();
 
-                        this.grid[i][j+1].tileValue(currVal);
-                        this.grid[i][j+1].tileValue(0);
+                            this.grid[i][k+1].tileValue(currVal);
+                            this.grid[i][k+1].tileValue(0);
+                            k++;
+                        }
+
+                    }
+                } 
+            }
+        }
+    }
+
+    goLeft() {
+        for(var j=0; j<3; j++){
+            for(var i=0; i<4; i++){
+                if(this.grid[i][j].tileValue() !== 0){
+                    var k = j;
+                    while(k !== 0 && (this.grid[i][k-1].tileValue() == 0 || this.grid[i][k-1].tileValue() == this.grid[i][k].tileValue())){
+                        if(this.grid[i][k-1].tileValue() == this.grid[i][k].tileValue()){
+                            var nextVal = this.grid[i][j-1].tileValue();
+    
+                            this.grid[i][k-1].tileValue(nextVal * 2);
+                            this.grid[i][k-1].tileValue(0);
+                            k--
+                        } else if (this.grid[i][k-1] == 0) {
+                            var currVal = this.grid[i][k];
+    
+                            this.grid[i][k-1].tileValue(currVal);
+                            this.grid[i][k+1].tileValue(0);
+                            k--;
+                        }
                     }
                 }
             }
         }
     }
 
-    goLeft() {
-
-    }
-
     goUp() {
+        for(var i = 0; i<4; i++){
+            for(var j = 0; j<4; j++){
+                if(this.grid[i][j].tileValue() != 0){
+                    var k = i;
+                    while(k !== 0 && (this.grid[k-1][j].tileValue() == 0 || this.grid[k][j].tileValue() == this.grid[k-1][j].tileValue())){
+                        if(this.grid[k][j].tileValue() == this.grid[k-1][j].titleValue()){
+                            var nextVal = this.grid[k-1][j].tileValue();
 
+                            this.grid[k-1][j].tileValue(nextVal * 2);
+                            this.grid[k][j].tileValue(0);
+                            k--;
+                        } else if(this.grid[k-1][j].tileValue() == 0){
+                            var currVal = this.grid[k][j].tileValue();
+
+                            this.grid[k-1][j].tileValue(currVal);
+                            this.grid[k][j].tileValue(0);
+                            k--;
+                        }
+                    }
+                }
+            }
+        }
     }
 
     goDown() {
