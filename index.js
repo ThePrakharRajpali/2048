c85a2b9ffe350ff82f7d44fa535d8c5bdd41eabf
Prakhar <rajpaliprakhar23@gmail.com>
2021-06-08 16:10:22 +0530

    Coded basic logic (Testing and debugging left)

--- index.js
+++ index.js
@@ -1,238 +1,231 @@
 var isGameOn = true;
-
 var grid = document.getElementsByTagName('td');
 var rightButton = document.getElementById('RightButton');
 var upButton = document.getElementById('UpButton');
 var leftButton = document.getElementById('LeftButton');
 var downButton = document.getElementById('DownButton');
-var gameOn = false;
 var gameScore = 0;  
 
-class Tile {
-    constructor(value, x, y){
-        this.value = value;
-        this.x = x;
-        this.y = y;
-    }
+var board = [
+    [0, 0, 0, 0], 
+    [0, 0, 0, 0], 
+    [0, 0, 0, 0], 
+    [0, 0, 0, 0]
+];
 
-    get tileValue() {
-        return this.value;
-    }
-
-    get tileX() {
-        return this.x;
-    }
-
-    get tileY() {
-        return this.y;
-    }
-
-    moveLeft() {
-        this.x--;
-    }
-
-    moveRight() {
-        this.x++;
-    }
-
-    moveUp() {
-        this.y--;
-    }
-
-    moveDown() {
-        this.y++
-    }
-
-    set tileValue(x) {
-        this.value = x;
-    }
+function showBoard(board, grid) {
+    grid.forEach((element, index) => {
+        var x = Math.floor(index / 4);
+        var y = index % 4;
+        element.innerHTML = board[x][y];
+    });
 }
 
-class Board {
-    constructor() {
-        this.grid = [];
-        for(var i =0; i<4; i++) {
-            var row = [];
-            for(var j =0; j<4; j++){
-                var newTile = new Tile(0, j, i);
-                row.push(newTile);
+function isFull(board) {
+    for(var i=0; i<4; i++){
+        for(var j=0; i<4; j++){
+            if(board[i][j] == 0){
+                return false;
             }
-            this.grid.push(row);
         }
     }
+    return true;
+}
 
-    addRandom() {
+function addRandom(board) {
+    if(!isFull(board)){
         var randomNum = Math.floor(Math.random() * 16);
-        var y = Math.floor(randomNum / 4);
-        var x = randomNum % 4;
-        while(this.grid[y][x].tileValue != 0){
-            var randomNum = Math.floor(Math.random() * 16);
-            var y = Math.floor(randomNum / 4);
-            var x = randomNum % 4;
-        }
-        this.grid[y][x].value = 2;
-    }
-
-    goRight() {
-        for(var j=3; j>=0; j--){
-            for(var i=0; i<4; i++){
-                if(this.grid[i][j].tileValue != 0){
-                    var k = j;
-                    while(k !=3 && (this.grid[i][k+1].tileValue == 0 || this.grid[i][k+1].tileValue == this.grid[i][k].tileValue)){
-                        if(this.grid[i][k].tileValue == this.grid[i][k+1].tileValue){
-                            var nextVal = this.grid[i][k+1].tileValue;
-                            this.grid[i][k+1].tileValue = nextVal * 2;
-                            this.grid[i][k].tileValue = 0;
-                        } else if(this.grid[i][k+1].tileValue == 0){
-                            var currVal = this.grid[i][k].tileValue;
-                            this.grid[i][k+1].tileValue = currVal;
-                            this.grid[i][k].tileValue = 0;
-                        }
-                    }
-                }
-            }
-        }
-        this.addRandom();
-    }
-
-    goLeft() {
-        for(var j=0; j<4; j++){
-            for(var i=0; i<4; i++){
-                if(this.grid[i][j].tileValue !== 0){
-                    var k = j;
-                    while(k !== 0 && (this.grid[i][k-1].tileValue == 0 || this.grid[i][k-1].tileValue == this.grid[i][k].tileValue)){
-                        if(this.grid[i][k-1].tileValue == this.grid[i][k].tileValue){
-                            var nextVal = this.grid[i][j-1].tileValue;
+        var x = Math.floor(randomNum / 4);
+        var y = randomNum % 4;
     
-                            this.grid[i][k-1].tileValue = nextVal * 2;
-                            this.grid[i][k].tileValue = 0;
-                            k--;
-                        } else if (this.grid[i][k-1].tileValue == 0) {
-                            var currVal = this.grid[i][k].tileValue;
+        while(board[x][y] != 0){
+            randomNum = Math.floor(Math.random() * 16);
+            x = Math.floor(randomNum / 4);
+            y = randomNum % 4;
+        }
     
-                            this.grid[i][k-1].tileValue = currVal;
-                            this.grid[i][k].tileValue = 0;
-                            k--;
-                        }
-                    }
-                }
+        board[x][y] = 2;
+    
+        console.log(`Added 2 at ${x} , ${y}`); 
+        return board;
+    } 
+    console.log('Board is full');
+    return board;
+}
+
+function swapTile(x1, y1, x2, y2, board){
+    if(board[x2][y2] == 0){
+        board[x2][y2] = board[x1][y1];
+        board[x1][y1] = 0;
+        console.log('Swap: Success');
+        return board;
+    } 
+    console.log('Swap: Failed');
+
+    return board;
+}
+
+function mergeTile(x1, y1, x2, y2, board) {
+    if(board[x2][y2] == board[x1][y1]){
+        board[x2][y2] *= 2;
+        board[x1][y1] = 0;
+        console.log('Merge: Success');
+        return board;
+    }
+    console.log('Merge: Failed');
+
+    return board;
+}
+
+function moveRight(board){
+    //move --> merge --> move
+    for(var i=0; i<4; i++){
+        var j=2;
+        while(j != 0){
+            if( j!= 3 && board[i][j] != 0 && board[i][j+1] == 0){
+                board = swapTile(i, j, i, j+1, board);
+                j++;
+            } else {
+                j--;
+            }
+            
+        }
+    } 
+
+    for(var i=0; i<4; i++){
+        var j=3;
+        while(j>0){
+            if(board[i][j] == board[i][j-1] && board[i][j] != 0){
+                board = mergeTile(i,j-1, i, j, board);
+            }
+            j--;
+        }
+    }
+
+    for(var i=0; i<4; i++){
+        var j=2;
+        while(j != 0){
+            if( j!= 3 && board[i][j] != 0 && board[i][j+1] == 0){
+                board = swapTile(i, j, i, j+1, board);
+                j++;
+            } else {
+                j--;
+            }
+            
+        }
+    } 
+    return board;
+}
+
+function moveLeft(board) {
+    for(var i=0; i<4; i++){
+        var j = 1;
+        while(j<4){
+            if(j!=0 && board[i][j] != 0 && board[i][j-1] == 0){
+                board = swapTile(i, j, i, j-1, board);
+                j--;
+            } else {
+                j++;
             }
         }
-        this.addRandom();
-
     }
 
-    goUp() {
-        for(var i = 0; i<4; i++){
-            for(var j = 0; j<4; j++){
-                if(this.grid[i][j].tileValue != 0){
-                    var k = i;
-                    while(k !== 0 && (this.grid[k-1][j].tileValue == 0 || this.grid[k][j].tileValue == this.grid[k-1][j].tileValue)){
-                        if(this.grid[k][j].tileValue == this.grid[k-1][j].titleValue){
-                            var nextVal = this.grid[k-1][j].tileValue;
+    for(var i=0; i<4; i++){
+        var j=0;
+        while(j<4){
+            if(board[i][j] == board[i][j+1] && board[i][j] !=0){
+                board = mergeTile(i, j+1, i, j, board);
+            }
+            j++;
+        }
+    }
 
-                            this.grid[k-1][j].tileValue = nextVal * 2;
-                            this.grid[k][j].tileValue = 0;
-                            k--;
-                        } else if(this.grid[k-1][j].tileValue == 0){
-                            var currVal = this.grid[k][j].tileValue;
-
-                            this.grid[k-1][j].tileValue = currVal;
-                            this.grid[k][j].tileValue = 0;
-                            k--;
-                        }
-                    }
-                }
+    for(var i=0; i<4; i++){
+        var j = 1;
+        while(j<3){
+            if(j!=0 && board[i][j] != 0 && board[i][j-1] == 0){
+                board = swapTile(i, j, i, j-1, board);
+                j--;
+            } else {
+                j++;
             }
         }
-        this.addRandom();
     }
+    return board;
+}
 
-    goDown() {
-        for(var i=3; i>=0; i--){
-            for(var j=0; j<4; j++){
-                if(this.grid[i][j].tileValue != 0){
-                    var k = i;
-                    while(k !== 3 && (this.grid[k+1][j].tileValue == 0 || this.grid[k+1][j].tileValue == this.grid[k][j].tileValue )){
-                        if(this.grid[k][j].tileValue == this.grid[k+1][j].tileValue){
-                            var nextVal = this.grid[k+1][j].tileValue;
-
-                            this.grid[k+1][j].tileValue = nextVal * 2;
-                            this.grid[k][j].tileValue = 0;
-                            k++;
-                        } else if (this.grid[k+1][j].tileValue == 0){
-                            var currVal = this.grid[k][j].tileValue;
-
-                            this.grid[k+1][j].tileValue = currVal;
-                            this.grid[k][j].tileValue = 0;
-                            k++;
-                        }
-                    }
-                }
+function moveUp(board) {
+    for(var j=0; j<4; j++){
+        var i =1;
+        while(i<4){
+            if(i != 0 && baord[i][j] != 0 && board[i-1][j] == 0){
+                board = swapTile(i, j, i-1, j, board);
+                i--;
+            } else {
+                i++;
             }
         }
-        this.addRandom();
-
     }
 
-    get getGrid(){
-        return this.grid;
+    for(var j=0; j<4; j++){
+        var i=0;
+        while(i<3){
+            if(board[i][j] == board[i+1][j] && board[i][j] !=0){
+                board = mergeTile(i+1, j, i, j, board);
+            }
+            i++;
+        }
     }
 
-    initializeGame(){
-        var randomNum = Math.floor(Math.random() * 16);
-        var y = Math.floor(randomNum / 4);
-        var x = randomNum % 4;
-
-        this.grid[y][x].value = 2;
+    for(var j=0; j<4; j++){
+        var i =1;
+        while(i<4){
+            if(i != 0 && baord[i][j] != 0 && board[i-1][j] == 0){
+                swapTile(i, j, i-1, j, board);
+                i--;
+            } else {
+                i++;
+            }
+        }
     }
+
+    return board;
 }
 
-var board = new Board();
-board.initializeGame();
-
-for(var i = 0; i<16; i++){
-    var row = Math.floor(i / 4);
-    var col = i % 4;
-    grid[i].innerHTML = board.getGrid[row][col].tileValue
-}
-
-upButton.onclick = () => {
-    board.goUp();
-    for(var i = 0; i<16; i++){
-        var row = Math.floor(i / 4);
-        var col = i % 4;
-        grid[i].innerHTML = board.getGrid[row][col].tileValue
+function moveDown(board) {
+    for(var j=0; j<4; j++){
+        var i = 2;
+        while(i>=0){
+            if(i!=3 && board[i][j] !=0 && board[i+1][j] == 0){
+                board = swapTile(i,j,i+1,j, board);
+                i++;
+            } else {
+                i--;
+            }
+        }
     }
-}
 
-downButton.onclick = () => {
-    board.goDown();
-    for(var i = 0; i<16; i++){
-        var row = Math.floor(i / 4);
-        var col = i % 4;
-        grid[i].innerHTML = board.getGrid[row][col].tileValue
+    for(var j=0; j<4; j++){
+        var i=3;
+        while(i>0){
+            if(board[i][j] == board[i-1][j] && board[i][j] != 0){
+                board = mergeTile(i-1, j, i, j, board);
+            }
+            i++;
+        }
     }
-}
 
-rightButton.onclick = () => {
-    board.goRight();
-    for(var i = 0; i<16; i++){
-        var row = Math.floor(i / 4);
-        var col = i % 4;
-        grid[i].innerHTML = board.getGrid[row][col].tileValue
+    for(var j=0; j<4; j++){
+        var i = 2;
+        while(i>=0){
+            if(i!=3 && board[i][j] !=0 && board[i+1][j] == 0){
+                board = swapTile(i,j,i+1,j, board);
+                i++;
+            } else {
+                i--;
+            }
+        }
     }
+
+    return board;
 }
-
-leftButton.onclick = () => {
-    board.goLeft();
-    for(var i = 0; i<16; i++){
-        var row = Math.floor(i / 4);
-        var col = i % 4;
-        grid[i].innerHTML = board.getGrid[row][col].tileValue
-    }
-}
-
-
-// not end of tile
-// should be empty or same tile
